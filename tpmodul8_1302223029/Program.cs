8c76a1c87c7c166060888a9ab322283371a649c6
asyifaa26 <153969653+asyifaa26@users.noreply.github.com>
2024-04-15 04:59:44 +0700

    Menyelesaikan program dengan runtime configuration

--- tpmodul8_1302223029/Program.cs
+++ tpmodul8_1302223029/Program.cs
@@ -1,2 +1,172 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+public class Program
+{
+    // Class Config berfungsi untuk menampung konfigurasi
+    public class Config
+    {
+        // Atribute
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        // Constructor kosong untuk deserialisasi
+        public Config() { }
+
+        // Menerima masukan dari deserialisasi
+        public Config(string suhu, int batasDemam, string pesanDitolak, string pesanDiterima)
+        {
+            satuan_suhu = suhu;
+            batas_hari_demam = batasDemam;
+            pesan_ditolak = pesanDitolak;
+            pesan_diterima = pesanDiterima;
+        }
+    }
+
+    // Class CovidConfig untuk membaca dan menulis file konfigurasi
+    public class CovidConfig
+    {
+        // Deklarasi nama variabel configuration dengan tipe data Config
+        public Config configuration;
+
+        // Pada filePath ini SENGAJA tidak diarahkan ke file JSON dengan benar agar code Catch dibawah ini dijalankan
+        // File Baru akan terbantuk dengan sendirinya dan dinamai sesuai nama file ujungnya yakni Covid_Config.json
+        public const string filePath = "D:\\TELKOM\\SEMESTER 4\\PRATIKUM KPL\\tpmodul8_1302223029\\tpmodul8_1302223029\\bin\\Debug\\net8.0\\Covid_config.json";
+
+        // Method untuk membaca dan menulis file Covid_Config.json baru jika belum ada/dibuat
+        public CovidConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+        // Deklarasi nilai Default-nya dari CovidConfig
+        public void SetDefault()
+        {
+            configuration = new Config("celcius", 14, "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                "Anda diperbolehkan masuk ke dalam gedung ini");
+        }
+
+        // Method untuk membaca file configurasi
+        private Config ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            configuration = JsonSerializer.Deserialize<Config>(configJsonData);
+            return configuration;
+        }
+
+        // Method untuk menulis file configurasi
+        public void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String jsonString = JsonSerializer.Serialize(configuration, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        // Method untu mengubah satuan 
+        public void UbahSatuan()
+        {
+            if (configuration.satuan_suhu == null)
+            {
+                configuration.satuan_suhu = "celcius";
+            }
+            else if (configuration.satuan_suhu == "celcius")
+            {
+                configuration.satuan_suhu = "fahrenheit";
+            }
+            else if (configuration.satuan_suhu == "fahrenheit")
+            {
+                configuration.satuan_suhu = "celcius";
+            }
+        }
+    }
+
+        public static void Main(string[] args)
+    {
+        // Memanggil Class CovidConfig untuk integrasi json, dan runtime configuration
+        CovidConfig Konfig = new CovidConfig();
+
+        // Menerima inputan dalam satuan suhu celcius
+        Console.WriteLine("Berapa suhu badan anda saat ini?, Dalam nilai " + Konfig.configuration.satuan_suhu);
+        double inputSuhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+        int inputHari = Convert.ToInt32(Console.ReadLine());
+
+        // Pengkondisian pesan yang ingin ditampilkan
+        if (Konfig.configuration.satuan_suhu == "celcius")
+        {
+            if ((inputSuhu >= 36.5 || inputSuhu <= 37.5) && inputHari < Konfig.configuration.batas_hari_demam)
+            {
+                Console.WriteLine(Konfig.configuration.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(Konfig.configuration.pesan_ditolak);
+            }
+        }
+        else if (Konfig.configuration.satuan_suhu == "fahrenheit")
+        {
+            if ((inputSuhu >= 97.7 || inputSuhu <= 99.5) && inputHari < Konfig.configuration.batas_hari_demam)
+            {
+                Console.WriteLine(Konfig.configuration.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(Konfig.configuration.pesan_ditolak);
+
+            }
+        }
+
+        // Menjalankan method UbahSatuan pada class CovidConfig
+        Konfig.UbahSatuan();
+        Console.WriteLine();
+
+        // Menerima inputan 2 dalam satuan suhu fahreinhet (diubah oleh method UbahSatuan)
+        Console.WriteLine("Berapa suhu badan anda saat ini?, Dalam nilai " + Konfig.configuration.satuan_suhu);
+        inputSuhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+        inputHari = Convert.ToInt32(Console.ReadLine());
+
+        // Pengkondisian pesan yang ingin ditampilkan
+        if (Konfig.configuration.satuan_suhu == "celcius")
+        {
+            if ((inputSuhu >= 36.5 || inputSuhu <= 37.5) && inputHari < Konfig.configuration.batas_hari_demam)
+            {
+                Console.WriteLine(Konfig.configuration.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(Konfig.configuration.pesan_ditolak);
+            }
+        }
+        else if (Konfig.configuration.satuan_suhu == "fahrenheit")
+        {
+            if ((inputSuhu >= 97.7 || inputSuhu <= 99.5) && inputHari < Konfig.configuration.batas_hari_demam)
+            {
+                Console.WriteLine(Konfig.configuration.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(Konfig.configuration.pesan_ditolak);
+
+            }
+        }
+    }
+}
--- tpmodul8_1302223029/obj/Debug/net8.0/tpmodul8_1302223029.AssemblyInfo.cs
+++ tpmodul8_1302223029/obj/Debug/net8.0/tpmodul8_1302223029.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul8_1302223029")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f9f1fc4f144c260a9d1f9f79617841040b5250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea281bf75060a6f288d7230f4b85d990bcd7bf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul8_1302223029")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul8_1302223029")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
